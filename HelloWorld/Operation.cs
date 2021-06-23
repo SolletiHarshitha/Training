745e4b614f48abb5ff846faaff4c7cd2e21e3e1d
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-23 23:23:24 +0530

    Adding ?HelloWorld file

---
+++ HelloWorld/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Operation
+    {
+            public void Addition(int var1,int var2)
+            {
+                Console.WriteLine("Addition :"+ (var1+var2));
+            }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.Addition(5,2);
+            Console.WriteLine("Hello");
+            Console.WriteLine("HI");
+        }
+    }
+}
