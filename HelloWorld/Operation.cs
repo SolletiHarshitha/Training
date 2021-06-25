fdd8adb3aec3114b824af6ce0ec5a8c30c9dad11
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-25 10:09:20 +0530

    Adding operation.cs file

--- HelloWorld/Operation.cs
+++ HelloWorld/Operation.cs
@@ -6,9 +6,14 @@ namespace HelloWorld
 {
     class Operation
     {
-            public void Addition(int var1,int var2)
-            {
-                Console.WriteLine("Addition :"+ (var1+var2));
-            }
+        /// <summary>
+        /// creating bestcoding sample class
+        /// </summary>
+        /// <param name="var1"></param>
+        /// <param name="var2"></param>
+        public void Addition(int var1,int var2)
+        {
+            Console.WriteLine("Addition :"+ (var1+var2));
+        }
     }
 }
